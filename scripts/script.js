b4ce46cadfcf30d49fbd19d0d071748a7db0da94
Семья <kaloshaviktar1@gmail.com>
2023-05-04 14:26:43 +0200

    add file scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function calculate(radius) {
  
     let area = (radius**2) * 3.14;
-    const p_result = document.querySelector(".form_result")
+    const p_result = document.querySelector(".form__result")
     p_result.innerHTML = "Площадь равна: " + area;
 }
 calculate(r)
