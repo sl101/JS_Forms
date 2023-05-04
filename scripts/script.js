5e7fc3ec51ac3fb636fe5258dadf1e16457f9bcc
Семья <kaloshaviktar1@gmail.com>
2023-05-04 14:28:28 +0200

    убрал скобки

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function calculate(radius) {
  
-    let area = (radius**2) * 3.14;
+    let area = radius**2 * 3.14;
     const p_result = document.querySelector(".form__result")
     p_result.innerHTML = "Площадь равна: " + area;
 }
