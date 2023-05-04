0a2149636efb9ba022c1597578842db4d005b53c
Sergii <wheelyhog@gmail.com>
2023-05-04 14:57:30 +0200

    fix bug

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
-const Square = () => {
+let form = document.querySelector('.form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
   const r = document.querySelector('.form__input').value
+  square(r);
+})
+
+const square = (r) => {
+
   let result = 3.1415 * r ** 2
   document.querySelector('.form__result').innerText = result
 }
